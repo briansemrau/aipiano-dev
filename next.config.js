1eff3331dde0e79d7af71942246af1d9a31cf595
Brian Semrau <brian.semrau@gmail.com>
2023-06-01 19:02:27 -0400

    hopefully fix export

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    output: 'export',
     webpack: (config) => {
         config.resolve.alias.canvas = false
         // allow loading audio files
