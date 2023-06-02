20748398df117c69551d9eb50fe072dbdcf70188
Brian Semrau <brian.semrau@gmail.com>
2023-06-01 20:03:55 -0400

    add export basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-    assetPrefix: isProd ? '/aipiano' : '',
+    basePath: isProd ? '/aipiano' : '',
+    assetPrefix: isProd ? '/aipiano/' : '',
     images: {
         unoptimized: true,
     },
