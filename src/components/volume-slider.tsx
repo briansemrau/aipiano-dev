cb2e2a43c1c7f969615fdc1234c8468bbea94277
Brian Semrau <brian.semrau@gmail.com>
2023-06-03 16:48:24 -0400

    store volume/mute to localStorage

--- src/components/volume-slider.tsx
+++ src/components/volume-slider.tsx
@@ -5,10 +5,17 @@ import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
 export const VolumeSlider = () => {
   const [volume, setVolume] = useState(0.5);
   const [muted, setMuted] = useState(false);
-  
+
+  useEffect(() => {
+    setVolume(Number(localStorage.getItem("volume") || 0.5));
+    setMuted(Boolean(localStorage.getItem("muted") || false));
+  }, [])
+
   useEffect(() => {
     Tone.getDestination().mute = muted;
     Tone.getDestination().output.gain.value = volume;
+    localStorage.setItem("volume", String(volume));
+    localStorage.setItem("muted", String(muted));
   }, [volume, muted]);
 
   function handleVolumeChange(event: React.ChangeEvent<HTMLInputElement>) {
